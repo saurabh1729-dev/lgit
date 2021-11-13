1d5eb6afa30d80db0260c80d79466e28e234ccea
saurabh chaudhary <saurabh33073@gmail.com>
2021-11-13 10:36:44 +0530

    v1 of file

---
+++ prc.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string st;
+        cin>>st;
+        vector<int> zeros;
+        vector<int> ones;
+        for(int i=0;i<n;i++){
+            if(st[i]=='0') zeros.push_back(i);
+            else ones.push_back(i);
+        }
+        bool flag=true;
+        while(flag){
+            
+        }
+    }
+}
